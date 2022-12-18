a90fe48c6dc90a29eac8bea01b4d2b263bc8ed3a
Patrice VIDEGNON <pvidegnon@gmail.com>
2022-12-18 13:34:30 +0100

    Fin de ce grid

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Destinations from "./components/destinations";
 function App() {
   return(
 <div className=" grid place-items-center min-h-screen"> 
-
 <div className="grid gap-4 p-4 max-w-5xl xs:grid-cols-2 md:grid-cols-4">
 <h1 className=" text-4xl font-extrabold  xs:col-span-2 xs:grid xs:grid-cols-2 xs:gap-4 md:col-span-3 md:text-5xl md:grid-cols-3 " ><span className=" bg-gray-400  md: col-span-1">Grid Layout By TailWindCss</span></h1>
 <p className="xs:row-start-2 xs:col-start-2 xs:self-center md:col-start-1 md:col-span-2 pr-12 text-lg">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam sapiente nihil aperiam! Aut, nihil iste itaque beatae, eaque sapiente non officia fugit quasi, voluptate labore facere minima expedita rerum? Incidunt.</p>
