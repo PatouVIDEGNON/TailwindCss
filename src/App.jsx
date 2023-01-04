aee972fc1de9d5d90291957c0338f1ec1340e4e3
Patrice VIDEGNON <pvidegnon@gmail.com>
2023-01-04 17:26:57 +0100

    grid avec un sideBar

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,18 @@ function App() {
   return(
 <div className="min-h-screen flex items-center bg-teal-500">
   <div className="flex-1  max-w-4xl mx-auto p-10">
+
   <ul className="grid grid-rows-4 grid-cols-6 gap-8 grid-flow-row"> 
             <li className="  bg-gray-800 rounded-lg shadow-xl "><div className="h-24"> grid grid-cols-1 gap-4  </div></li>
-            <li className=" col-start-1 row-start-1 row-span-2 col-span-2 bg-pink-600 rounded-lg shadow-xl"><div className="h-24">sm:grid-cols-2 sm:gap-4 </div></li>
+            <li className=" bg-pink-600 rounded-lg shadow-xl"><div className="h-24">sm:grid-cols-2 sm:gap-4 </div></li>
             <li className="  bg-white rounded-lg shadow-xl"><div className="h-24">md:grid-cols-3 md:gap-8</div></li>
             <li className="  bg-white rounded-lg shadow-xl"><div className="h-24"></div></li>
             <li className="  bg-white rounded-lg shadow-xl"><div className="h-24"></div></li>
             <li className="  bg-white rounded-lg shadow-xl"><div className="h-24"></div></li>
             <li className="  bg-pink-600 rounded-lg shadow-xl"><div className="h-24"></div></li>
-            <li className="  bg-blue-900 rounded-lg shadow-xl"><div className="h-24"></div></li>
+            <li className=" lg:col-start-1 lg:row-start-1 lg:row-span-3 lg:col-span-2 bg-blue-900 rounded-lg shadow-xl"><div className="h-24"></div></li>
             <li className=" bg-white rounded-lg shadow-xl"><div className="h-24"></div></li>
+            <li className="  bg-white rounded-lg shadow-xl"><div className="h-24"></div></li>
             <li className="  bg-white rounded-lg shadow-xl"><div className="h-24"></div></li>
             <li className="  bg-white rounded-lg shadow-xl"><div className="h-24"></div></li>
             <li className="  bg-white rounded-lg shadow-xl"><div className="h-24"></div></li>
