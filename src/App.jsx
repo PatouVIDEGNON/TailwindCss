152bb4308fb9623c2de270af4b2e22f021bde205
Patrice VIDEGNON <pvidegnon@gmail.com>
2023-01-04 17:15:57 +0100

    grid avec grid-flow-col-dense qui permet d'occuper les espaces vide dans le sens des columns

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ function App() {
   return(
 <div className="min-h-screen flex items-center bg-teal-500">
   <div className="flex-1  max-w-4xl mx-auto p-10">
-  <ul className="grid grid-rows-4 grid-cols-6 gap-8 grid-flow-row-dense"> 
-            <li className=" col-span-4 bg-gray-800 rounded-lg shadow-xl "><div className="h-24"> grid grid-cols-1 gap-4  </div></li>
-            <li className=" col-span-3 bg-pink-600 rounded-lg shadow-xl"><div className="h-24">sm:grid-cols-2 sm:gap-4 </div></li>
+  <ul className="grid grid-rows-4 grid-cols-6 gap-8 grid-flow-col-dense"> 
+            <li className="  bg-gray-800 rounded-lg shadow-xl "><div className="h-24"> grid grid-cols-1 gap-4  </div></li>
+            <li className=" row-span-4 bg-pink-600 rounded-lg shadow-xl"><div className="h-24">sm:grid-cols-2 sm:gap-4 </div></li>
             <li className="  bg-yellow-400 rounded-lg shadow-xl"><div className="h-24">md:grid-cols-3 md:gap-8</div></li>
             <li className="  bg-yellow-400 rounded-lg shadow-xl"><div className="h-24"></div></li>
             <li className="  bg-white rounded-lg shadow-xl"><div className="h-24"></div></li>
